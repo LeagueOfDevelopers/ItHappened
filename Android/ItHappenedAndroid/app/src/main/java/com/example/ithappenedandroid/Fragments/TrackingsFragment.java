d918aef5aab82b68067ea8f260c91c9a589f7367
kenny3run <31897812+kenny3run@users.noreply.github.com>
2017-12-02 20:16:28 +0300

    add second activity

---
+++ Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/Fragments/TrackingsFragment.java
@@ -0,0 +1,22 @@
+package com.example.ithappenedandroid.Fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TrackingsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
