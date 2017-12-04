9b2aac14e7d8b42a4ecc708771262f6ca7eb090d
Unknown <dedalexij@mail.ru>
2017-12-04 17:00:00 +0300

    tracking outline

---
+++ Backend/ItHappened/ItHappenedDomain/Domain/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItHappenedDomain.Domain
+{
+  class Comment : Customization
+  {
+    public Comment(string customizationName)
+    {
+      CustomizationName = customizationName;
+    }
+
+    private string _comment;
+  }
+}
---
+++ Backend/ItHappened/ItHappenedDomain/Domain/Scale.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ItHappenedDomain.Domain
+{
+  class Scale : Customization
+  {
+    public Scale(string customizationName)
+    {
+      CustomizationName = customizationName;
+    }
+
+    private int _scale;
+  }
+}
