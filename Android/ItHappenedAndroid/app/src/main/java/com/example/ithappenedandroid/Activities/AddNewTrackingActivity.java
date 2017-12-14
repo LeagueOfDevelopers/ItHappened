850ad93d1e00bd49b73ccaf3f802e3e756915254
kenny3run <31897812+kenny3run@users.noreply.github.com>
2017-12-14 17:53:12 +0300

    AddNewTracking activity. Design 6

--- Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/Activities/AddNewTrackingActivity.java
+++ Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/Activities/AddNewTrackingActivity.java
@@ -1,9 +1,12 @@
 package com.example.ithappenedandroid.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -32,6 +35,20 @@ public class AddNewTrackingActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addnewtracking);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                startActivity(new Intent(this, TrackingActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
