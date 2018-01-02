37591d5f3931f8ade1bdc528d30761509e83fc89
kenny3run <31897812+kenny3run@users.noreply.github.com>
2018-01-02 18:36:59 +0300

    Add statistics for comment and rating

--- Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/Fragments/StatisticsFragment.java
+++ Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/Fragments/StatisticsFragment.java
@@ -3,17 +3,35 @@ package com.example.ithappenedandroid.Fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.ithappenedandroid.Infrastructure.ITrackingRepository;
 import com.example.ithappenedandroid.R;
+import com.example.ithappenedandroid.Recyclers.StatisticsAdapter;
+import com.example.ithappenedandroid.StaticInMemoryRepository;
 
 public class StatisticsFragment extends Fragment {
+
+    ITrackingRepository trackingCollection = StaticInMemoryRepository.getInstance();
+    RecyclerView statisticsRecycler;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_statistics, null);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        statisticsRecycler = (RecyclerView) view.findViewById(R.id.statisticsRV);
+        statisticsRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+        StatisticsAdapter adapter = new StatisticsAdapter(trackingCollection.GetTrackingCollection(), getActivity());
+        statisticsRecycler.setAdapter(adapter);
+
+    }
 }
--- Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/Recyclers/StatisticsAdapter.java
+++ Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/Recyclers/StatisticsAdapter.java
@@ -7,9 +7,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.ithappenedandroid.Application.TrackingService;
+import com.example.ithappenedandroid.Domain.Event;
 import com.example.ithappenedandroid.Domain.Tracking;
 import com.example.ithappenedandroid.Domain.TrackingCustomization;
+import com.example.ithappenedandroid.Infrastructure.ITrackingRepository;
 import com.example.ithappenedandroid.R;
+import com.example.ithappenedandroid.StaticInMemoryRepository;
 import com.github.mikephil.charting.charts.LineChart;
 
 import java.util.List;
@@ -17,21 +21,14 @@ import java.util.List;
 public class StatisticsAdapter extends RecyclerView.Adapter<StatisticsAdapter.ViewHolder> {
 
     List<Tracking> trackings;
-    List<Tracking> trackingsWithScale;
-    List<Tracking> trackingsWithRating;
     Context context;
+
+    ITrackingRepository trackingCollection = StaticInMemoryRepository.getInstance();
+    TrackingService trackingService = new TrackingService("textUser", trackingCollection);
 
 
     public StatisticsAdapter(List<Tracking> trackings, Context context) {
         this.trackings = trackings;
-        for(Tracking tracking : trackings){
-            if(tracking.GetRatingCustomization()== TrackingCustomization.Required || tracking.GetRatingCustomization()== TrackingCustomization.Optional){
-                trackingsWithRating.add(tracking);
-            }
-            if(tracking.GetScaleCustomization()== TrackingCustomization.Required || tracking.GetScaleCustomization()== TrackingCustomization.Optional){
-                trackingsWithScale.add(tracking);
-            }
-        }
         this.context = context;
     }
 
@@ -45,6 +42,44 @@ public class StatisticsAdapter extends RecyclerView.Adapter<StatisticsAdapter.Vi
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
 
+        final Tracking tracking = trackings.get(position);
+        List<Event> events = tracking.GetEventCollection();
+
+        int commentCount = 0;
+        double avrgRatingNumber = 0;
+        int ratingCount = 0;
+
+        viewHolder.trackingTitle.setText(tracking.GetTrackingName());
+
+        if(tracking.GetCommentCustomization() == TrackingCustomization.None){
+            viewHolder.commentCount.setText("У этого отслеживания нет комментариев");
+        }else{
+            for(int i = 0;i<events.size();i++){
+                if(events.get(i).GetComment()!=null){
+                    commentCount++;
+                }
+            }
+            viewHolder.commentCount.setText(""+commentCount);
+        }
+
+
+        if(tracking.GetRatingCustomization()==TrackingCustomization.None){
+            viewHolder.avrgRating.setText("У этого отслеживания нет оценок");
+        }else {
+            for (int i = 0; i < events.size(); i++) {
+                if (events.get(i).GetRating() != null) {
+                    ratingCount++;
+                    avrgRatingNumber += events.get(i).GetRating().GetRatingValue();
+                }
+            }
+            if (ratingCount == 0) {
+                viewHolder.avrgRating.setText("0");
+            } else {
+                avrgRatingNumber/=ratingCount;
+                viewHolder.avrgRating.setText(""+avrgRatingNumber);
+            }
+        }
+
     }
 
     @Override
@@ -56,11 +91,15 @@ public class StatisticsAdapter extends RecyclerView.Adapter<StatisticsAdapter.Vi
 
         TextView trackingTitle;
         LineChart scaleChart;
+        TextView commentCount;
+        TextView avrgRating;
 
         public ViewHolder(View itemView) {
             super(itemView);
             scaleChart = (LineChart) itemView.findViewById(R.id.graphForScale);
-            trackingTitle = (TextView) itemView.findViewById(R.id.TracingTitle);
+            trackingTitle = (TextView) itemView.findViewById(R.id.TrackingTitleForStatistics);
+            commentCount = (TextView) itemView.findViewById(R.id.countOfComments);
+            avrgRating = (TextView) itemView.findViewById(R.id.avrgRating);
         }
     }
 
