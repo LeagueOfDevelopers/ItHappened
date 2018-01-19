31f63836878ab2aaa4ad515e13890e2b6d2543a1
kenny3run <31897812+kenny3run@users.noreply.github.com>
2018-01-19 13:05:11 +0300

    Create ItHappenedApi interface

---
+++ Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/Retrofit/ItHappenedApi.java
@@ -0,0 +1,22 @@
+package com.example.ithappenedandroid.Retrofit;
+
+import com.example.ithappenedandroid.Domain.Tracking;
+
+import java.util.List;
+import java.util.UUID;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+/**
+ * Created by Пользователь on 19.01.2018.
+ */
+
+public interface ItHappenedApi {
+
+    @POST("synchronization/{userId}")
+    Call<List<Tracking>> SynchronizeData(@Path("userId")UUID userId, @Body List<Tracking> trackingColletion);
+
+}
