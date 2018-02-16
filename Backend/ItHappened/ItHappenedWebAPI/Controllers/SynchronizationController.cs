84ee51fabb58a2d43f993a48eac5e4efa64472b4
dedalexij <dedalexij@mail.ru>
2018-02-16 17:54:20 +0300

    deleted not existing reference

--- Backend/ItHappened/ItHappenedWebAPI/Controllers/SynchronizationController.cs
+++ Backend/ItHappened/ItHappenedWebAPI/Controllers/SynchronizationController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using ItHappenedDomain.Domain;
 using ItHappenedDomain.Models;
-using ItHappenedWebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
