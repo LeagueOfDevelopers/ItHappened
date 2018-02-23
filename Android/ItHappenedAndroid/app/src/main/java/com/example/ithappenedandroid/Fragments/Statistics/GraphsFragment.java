2ab1aba0d825b3fca5508092b1f1e9fdd659f5d8
kenny3run <31897812+kenny3run@users.noreply.github.com>
2018-02-23 21:22:33 +0300

    Create hashmaps

--- Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/Fragments/Statistics/GraphsFragment.java
+++ Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/Fragments/Statistics/GraphsFragment.java
@@ -7,14 +7,61 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Spinner;
 
+import com.example.ithappenedandroid.Domain.Tracking;
+import com.example.ithappenedandroid.Gui.MultiSpinner;
+import com.example.ithappenedandroid.Infrastructure.ITrackingRepository;
 import com.example.ithappenedandroid.R;
+import com.example.ithappenedandroid.StaticInMemoryRepository;
+import com.example.ithappenedandroid.StatisticsHelpers.GraphStatisticsHelper;
+import com.github.mikephil.charting.charts.LineChart;
+
+import java.util.UUID;
 
 public class GraphsFragment extends Fragment {
+
+    GraphStatisticsHelper helper;
+    GraphTimeTypes timeTypes;
+    Button addParams;
+    LineChart graph;
+    Spinner graphType;
+    MultiSpinner trackingsSpinner;
+    ITrackingRepository collection;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_graphs, null);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        graph = (LineChart) getActivity().findViewById(R.id.graph);
+        graphType = (Spinner) getActivity().findViewById(R.id.typeSpinner);
+        trackingsSpinner = (MultiSpinner) getActivity().findViewById(R.id.trackingsGraphSpinner);
+        addParams = (Button) getActivity().findViewById(R.id.addParams);
+        collection = new StaticInMemoryRepository(getActivity().getApplicationContext()).getInstance();
+
+        timeTypes = GraphTimeTypes.ALLTIME;
+
+        Tracking tracking = collection.GetTracking(UUID.fromString(getActivity().getIntent().getStringExtra("id")));
+
+        helper = new GraphStatisticsHelper(tracking);
+
+    }
+
+    private void initLineChart(LineChart graph, GraphTimeTypes timeTypes){
+
+        switch (timeTypes){
+            case ALLTIME:
+        }
+
+    }
+
+    private void intLineChartMultiTrackings(LineChart graph, GraphTimeTypes timeTypes){
+
     }
 }
--- Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/StatisticsHelpers/GraphStatisticsHelper.java
+++ Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/StatisticsHelpers/GraphStatisticsHelper.java
@@ -5,6 +5,7 @@ import com.example.ithappenedandroid.Domain.Tracking;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.TimeZone;
 
@@ -15,20 +16,38 @@ public class GraphStatisticsHelper {
 
     public GraphStatisticsHelper(Tracking tracking) {
         this.tracking = tracking;
-        for(Event event : tracking.GetEventCollection()){
-            if(!event.GetStatus()){
+        for (Event event : tracking.GetEventCollection()) {
+            if (!event.GetStatus()) {
                 eventsCollection.add(event);
             }
         }
     }
 
-    public int allTimesGraphData(){
-        return eventsCollection.size();
+    public LinkedHashMap<Date, Integer> allTimesGraphData() {
+        LinkedHashMap<Date, Integer> count = new LinkedHashMap<>();
+        boolean flag = false;
+
+        for (Event event : eventsCollection) {
+                if (count.size() == 0) {
+                    count.put(getZeroTimeDate(event.GetEventDate()), 1);
+                } else {
+                    for (int i = 0; i < count.size(); i++) {
+                        if (count.containsKey(getZeroTimeDate(event.GetEventDate()))) {
+                            int iterationCount = count.get(getZeroTimeDate(event.GetEventDate())) + 1;
+                            count.put(getZeroTimeDate(event.GetEventDate()), iterationCount);
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (!flag) {
+                        count.put(getZeroTimeDate(event.GetEventDate()), 1);
+                    }
+                }
+            }
+        return count;
     }
 
-    public int lastWeekGraphData(){
-        int lastWeekCount = 0;
-
+    public LinkedHashMap<Date, Integer> lastWeekGraphData() {
         Date date = new Date();
         Calendar c = Calendar.getInstance();
         c.setTime(date);
@@ -38,19 +57,10 @@ public class GraphStatisticsHelper {
         c.add(Calendar.DATE, 6);
         Date end = c.getTime();
 
-        for(Event event : eventsCollection){
-            if(event.GetEventDate().compareTo(start) >= 0 && event.GetEventDate().compareTo(end) <= 0){
-                lastWeekCount++;
-            }
-        }
-
-        return lastWeekCount;
+        return createHashMap(start, end);
     }
 
-    public int lastMonthGraphData(){
-
-        int lastMonthCount = 0;
-
+    public LinkedHashMap<Date, Integer> lastMonthGraphData() {
         Date date = new Date();
 
         Calendar c = Calendar.getInstance();
@@ -60,18 +70,10 @@ public class GraphStatisticsHelper {
         Date start = c.getTime();
         Date end = Calendar.getInstance(TimeZone.getDefault()).getTime();
 
-        for(Event event : eventsCollection){
-            if(event.GetEventDate().compareTo(start) >= 0 && event.GetEventDate().compareTo(end) <= 0){
-                lastMonthCount++;
-            }
-        }
-
-        return lastMonthCount;
+        return createHashMap(start, end);
     }
 
-    public int lastThreeMonthGraphData(){
-
-        int lastThreeMonthCount = 0;
+    public LinkedHashMap<Date , Integer> lastThreeMonthGraphData() {
         Date date = new Date();
         Calendar c = Calendar.getInstance();
         c.setTime(date);
@@ -84,37 +86,22 @@ public class GraphStatisticsHelper {
         Date start = c.getTime();
         Date end = Calendar.getInstance(TimeZone.getDefault()).getTime();
 
-        for(Event event : eventsCollection){
-            if(event.GetEventDate().compareTo(start) >= 0 && event.GetEventDate().compareTo(end) <= 0){
-                lastThreeMonthCount++;
-            }
-        }
-        return lastThreeMonthCount;
-
+        return createHashMap(start, end);
     }
 
-    public int halfYearGraphData(){
-
-        int halfYearCount = 0;
-
+    public LinkedHashMap<Date , Integer> halfYearGraphData() {
         Date date = new Date();
         Calendar c = Calendar.getInstance();
         c.setTime(date);
-        c.add(Calendar.YEAR, -1/2);
+        c.add(Calendar.YEAR, -1 / 2);
         Date start = c.getTime();
         Date end = Calendar.getInstance(TimeZone.getDefault()).getTime();
 
-        for(Event event : eventsCollection){
-            if(event.GetEventDate().compareTo(start) >= 0 && event.GetEventDate().compareTo(end) <= 0){
-                halfYearCount++;
-            }
-        }
-
-        return halfYearCount;
+        return createHashMap(start, end);
 
     }
 
-    public int lastYearGraphData(){
+    public LinkedHashMap<Date, Integer> lastYearGraphData() {
 
         int lastYearCount = 0;
         Calendar c = Calendar.getInstance();
@@ -124,26 +111,55 @@ public class GraphStatisticsHelper {
         Date start = c.getTime();
         Date end = Calendar.getInstance(TimeZone.getDefault()).getTime();
 
-        for(Event event : eventsCollection){
-            if(event.GetEventDate().compareTo(start) >= 0 && event.GetEventDate().compareTo(end) <= 0){
-                lastYearCount++;
-            }
-        }
-
-        return lastYearCount;
+        return createHashMap(start, end);
     }
 
-    public int userPeriodGraphData(Date start, Date end){
+    public LinkedHashMap<Date , Integer> userPeriodGraphData(Date start, Date end) {
 
-        int userPeriodCount = 0;
+        return createHashMap(start, end);
 
-        for(Event event : eventsCollection){
-            if(event.GetEventDate().compareTo(start) >= 0 && event.GetEventDate().compareTo(end) <= 0){
-                userPeriodCount++;
+    }
+
+    private static Date getZeroTimeDate(Date fecha) {
+        Date res = fecha;
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setTime(fecha);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        res = calendar.getTime();
+
+        return res;
+    }
+
+    private LinkedHashMap<Date, Integer> createHashMap(Date start, Date end) {
+
+        LinkedHashMap<Date, Integer> count = new LinkedHashMap<>();
+        boolean flag = false;
+
+        for (Event event : eventsCollection) {
+            if (event.GetEventDate().compareTo(start) >= 0 && event.GetEventDate().compareTo(end) <= 0) {
+                if (count.size() == 0) {
+                    count.put(getZeroTimeDate(event.GetEventDate()), 1);
+                } else {
+                    for (int i = 0; i < count.size(); i++) {
+                        if (count.containsKey(getZeroTimeDate(event.GetEventDate()))) {
+                            int iterationCount = count.get(getZeroTimeDate(event.GetEventDate())) + 1;
+                            count.put(getZeroTimeDate(event.GetEventDate()), iterationCount);
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (!flag) {
+                        count.put(getZeroTimeDate(event.GetEventDate()), 1);
+                    }
+                }
             }
         }
 
-        return userPeriodCount;
-
+        return count;
     }
 }
