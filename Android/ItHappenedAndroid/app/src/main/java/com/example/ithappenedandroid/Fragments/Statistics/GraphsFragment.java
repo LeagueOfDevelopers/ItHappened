8c4f6f34152d65cf516bd8fb55e0f05c3296d8c0
kenny3run <31897812+kenny3run@users.noreply.github.com>
2018-02-27 17:43:27 +0300

    Add switch case for graph data type

--- Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/Fragments/Statistics/GraphsFragment.java
+++ Android/ItHappenedAndroid/app/src/main/java/com/example/ithappenedandroid/Fragments/Statistics/GraphsFragment.java
@@ -7,8 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
 import com.example.ithappenedandroid.Domain.Tracking;
@@ -37,6 +39,15 @@ import java.util.UUID;
 public class GraphsFragment extends Fragment {
 
     String[] spinnerHints = {"За все время", "За год", "За пол года", "За месяц", "За неделю", "За три месяца", "Ваш период"};
+    Enum[] spinnerTypes = {
+            GraphTimeTypes.ALLTIME,
+            GraphTimeTypes.LASTYEAR,
+            GraphTimeTypes.HALFYEAR,
+            GraphTimeTypes.LASTMONTH,
+            GraphTimeTypes.LASTWEEK,
+            GraphTimeTypes.THREEMONT,
+            GraphTimeTypes.USERTYPE
+    };
 
     GraphStatisticsHelper helper;
     GraphTimeTypes timeTypes;
@@ -45,6 +56,8 @@ public class GraphsFragment extends Fragment {
     Spinner graphType;
     MultiSpinner trackingsSpinner;
     ITrackingRepository collection;
+
+    RelativeLayout userPeriodLayout;
 
     @Nullable
     @Override
@@ -59,6 +72,8 @@ public class GraphsFragment extends Fragment {
         graphType = (Spinner) getActivity().findViewById(R.id.typeSpinner);
         trackingsSpinner = (MultiSpinner) getActivity().findViewById(R.id.trackingsGraphSpinner);
         addParams = (Button) getActivity().findViewById(R.id.addParams);
+        userPeriodLayout = (RelativeLayout) getActivity().findViewById(R.id.visibilityLayout);
+        userPeriodLayout.setVisibility(View.INVISIBLE);
         collection = new StaticInMemoryRepository(getActivity().getApplicationContext()).getInstance();
 
         timeTypes = GraphTimeTypes.ALLTIME;
@@ -73,6 +88,20 @@ public class GraphsFragment extends Fragment {
                 spinnerHints);
 
         graphType.setAdapter(adapter);
+
+        graphType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(spinnerTypes[i].equals(GraphTimeTypes.USERTYPE)){
+                    userPeriodLayout.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
         List<UUID> trackings  = new ArrayList<>();
         List<String> trackingNames = new ArrayList<>();
@@ -117,6 +146,93 @@ public class GraphsFragment extends Fragment {
                 createLineChart(graph, entries);
 
                 break;
+
+            case HALFYEAR:
+                LinkedHashMap<Date, Integer> halfYearGraphData = new GraphStatisticsHelper(tracking).halfYearGraphData();
+                ArrayList<Date> xDataHalfYear = new ArrayList<>(halfYearGraphData.keySet());
+                ArrayList<Integer> yDataHalfYear = new ArrayList<>(halfYearGraphData.values());
+                ArrayList<Entry> entriesHalfYear = new ArrayList<>();
+
+                for(int i =0;i<xDataHalfYear.size();i++){
+                    entriesHalfYear.add(new Entry(new Long(xDataHalfYear.get(i).getTime()).floatValue(), yDataHalfYear.get(i)));
+                }
+
+                Collections.sort(entriesHalfYear, new EntryXComparator());
+
+                createLineChart(graph, entriesHalfYear);
+
+                break;
+
+            case LASTWEEK:
+                LinkedHashMap<Date, Integer> lastWeekGraphData = new GraphStatisticsHelper(tracking).lastWeekGraphData();
+                ArrayList<Date> xDataLastWeek = new ArrayList<>(lastWeekGraphData.keySet());
+                ArrayList<Integer> yDataLastWeek = new ArrayList<>(lastWeekGraphData.values());
+                ArrayList<Entry> entriesLastWeek = new ArrayList<>();
+
+                for(int i =0;i<xDataLastWeek.size();i++){
+                    entriesLastWeek.add(new Entry(new Long(xDataLastWeek.get(i).getTime()).floatValue(), yDataLastWeek.get(i)));
+                }
+
+                Collections.sort(entriesLastWeek, new EntryXComparator());
+
+                createLineChart(graph, entriesLastWeek);
+
+                break;
+
+            case LASTYEAR:
+                LinkedHashMap<Date, Integer> lastYearGraphData = new GraphStatisticsHelper(tracking).lastYearGraphData();
+                ArrayList<Date> xDataLastYear = new ArrayList<>(lastYearGraphData.keySet());
+                ArrayList<Integer> yDataLastYear = new ArrayList<>(lastYearGraphData.values());
+                ArrayList<Entry> entriesLastYear = new ArrayList<>();
+
+                for(int i =0;i<xDataLastYear.size();i++){
+                    entriesLastYear.add(new Entry(new Long(xDataLastYear.get(i).getTime()).floatValue(), yDataLastYear.get(i)));
+                }
+
+                Collections.sort(entriesLastYear, new EntryXComparator());
+
+                createLineChart(graph, entriesLastYear);
+
+                break;
+
+            case LASTMONTH:
+                LinkedHashMap<Date, Integer> lastMonthGraphData = new GraphStatisticsHelper(tracking).lastMonthGraphData();
+                ArrayList<Date> xDataLastMonth = new ArrayList<>(lastMonthGraphData.keySet());
+                ArrayList<Integer> yDataLastMonth = new ArrayList<>(lastMonthGraphData.values());
+                ArrayList<Entry> entriesLastMonth = new ArrayList<>();
+
+                for(int i =0;i<xDataLastMonth.size();i++){
+                    entriesLastMonth.add(new Entry(new Long(xDataLastMonth.get(i).getTime()).floatValue(), yDataLastMonth.get(i)));
+                }
+
+                Collections.sort(entriesLastMonth, new EntryXComparator());
+
+                createLineChart(graph, entriesLastMonth);
+
+                break;
+
+            case THREEMONT:
+                LinkedHashMap<Date, Integer> threeMonthGraphData = new GraphStatisticsHelper(tracking).lastThreeMonthGraphData();
+                ArrayList<Date> xDataThreeMonth = new ArrayList<>(threeMonthGraphData.keySet());
+                ArrayList<Integer> yDataThreeMonth = new ArrayList<>(threeMonthGraphData.values());
+                ArrayList<Entry> entriesThreeMonth = new ArrayList<>();
+
+                for(int i =0;i<xDataThreeMonth.size();i++){
+                    entriesThreeMonth.add(new Entry(new Long(xDataThreeMonth.get(i).getTime()).floatValue(), yDataThreeMonth.get(i)));
+                }
+
+                Collections.sort(entriesThreeMonth, new EntryXComparator());
+
+                createLineChart(graph, entriesThreeMonth);
+
+                break;
+
+            case USERTYPE:
+                break;
+
+                
+                default:
+                    break;
         };
 
     }
