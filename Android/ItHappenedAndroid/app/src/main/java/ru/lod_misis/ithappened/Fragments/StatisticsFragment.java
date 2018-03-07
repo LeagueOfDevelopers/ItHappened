79d2b1f491fa845c8df6a4b7a2b9331676788521
kenny3run <31897812+kenny3run@users.noreply.github.com>
2018-03-07 18:54:51 +0300

    Create parent carusel for statistics fragment

--- Android/ItHappenedAndroid/app/src/main/java/ru/lod_misis/ithappened/Fragments/StatisticsFragment.java
+++ Android/ItHappenedAndroid/app/src/main/java/ru/lod_misis/ithappened/Fragments/StatisticsFragment.java
@@ -1,6 +1,7 @@
 package ru.lod_misis.ithappened.Fragments;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
@@ -13,28 +14,45 @@ import android.widget.TextView;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ViewListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ru.lod_misis.ithappened.Application.TrackingService;
+import ru.lod_misis.ithappened.Domain.Tracking;
 import ru.lod_misis.ithappened.Infrastructure.ITrackingRepository;
 import ru.lod_misis.ithappened.R;
 import ru.lod_misis.ithappened.Recyclers.StatisticsAdapter;
+import ru.lod_misis.ithappened.StaticInMemoryRepository;
 
 
 public class StatisticsFragment extends Fragment {
 
     RecyclerView trackingsRecycler;
     StatisticsAdapter trackAdpt;
-    TrackingService service;
-    ITrackingRepository collection;
 
     TextView hint;
 
     CarouselView carousel;
 
-    String[] testStrings = {"Общие данные","1","2","3","4"};
+    List<String> titles = new ArrayList<>();
+
+    ITrackingRepository trackingCollection;
+    TrackingService service;
+    List<Tracking> allTrackings = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        trackingCollection = new StaticInMemoryRepository(getActivity().getApplicationContext(),
+                getActivity().getSharedPreferences("MAIN_KEYS", Context.MODE_PRIVATE).getString("UserId", "")).getInstance();
+        service = new TrackingService(getActivity().getSharedPreferences("MAIN_KEYS", Context.MODE_PRIVATE).getString("UserId", ""), trackingCollection);
+        titles.add("Общая статистика");
+        for(Tracking tracking: service.GetTrackingCollection()){
+            if(!tracking.GetStatus()){
+                titles.add(tracking.GetTrackingName());
+                allTrackings.add(tracking);
+            }
+        }
         return inflater.inflate(ru.lod_misis.ithappened.R.layout.fragment_statistics, null);
     }
 
@@ -42,10 +60,12 @@ public class StatisticsFragment extends Fragment {
     public void onResume() {
         super.onResume();
         getActivity().setTitle("Статистика");
+
+
         carousel = (CarouselView) getActivity().findViewById(R.id.mainCarousel);
         carousel.setViewListener(viewListener);
-        carousel.setPageCount(5);
-        getActivity().setTitle(testStrings[carousel.getCurrentItem()]);
+        carousel.setPageCount(titles.size());
+        getActivity().setTitle(titles.get(0));
         carousel.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int i, float v, int i1) {
@@ -54,7 +74,7 @@ public class StatisticsFragment extends Fragment {
 
             @Override
             public void onPageSelected(int i) {
-                getActivity().setTitle(testStrings[i]);
+                getActivity().setTitle(titles.get(i));
             }
 
             @Override
@@ -70,7 +90,7 @@ public class StatisticsFragment extends Fragment {
         public View setViewForPosition(int position) {
 
             View customView = getActivity().getLayoutInflater().inflate(R.layout.view_statistics, null);
-            getActivity().setTitle(testStrings[position]);
+            getActivity().setTitle(titles.get(position));
             return customView;
         }
     };
