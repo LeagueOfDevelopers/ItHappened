65915da42576c893c41d536c924bfd3f7670f0d4
Boris Valdman <boris.valdman@live.ru>
2018-04-04 20:22:09 +0300

    refactor

--- Backend/ItHappened/ItHappenedWebAPI/Migrations/MigrateDB.cs
+++ Backend/ItHappened/ItHappenedWebAPI/Migrations/MigrateDB.cs
@@ -17,30 +17,14 @@ namespace ItHappenedWebAPI.Migrations
     public void Migrate()
     {
       var collection = db.GetCollection<OldUserModel>("Users");
-      var users = collection.FindAsync(a => true);
-      try
-      {
-        var result = users.Result;
-        if (result != null)
-        {
-          List<OldUserModel> userList = new List<OldUserModel>(result.ToList().FindAll(a => true));
-          List<User> newUserList = new List<User>();
+      var users = collection.Find(u => true).ToList();
+      
+      var newUserList = users?.Select(user => new User(user)).ToList();
 
-          foreach (var user in userList)
-          {
-            User newUser = new User(user);
-            newUserList.Add(newUser);
-          }
+      db.DropCollection("Users");
 
-          db.DropCollection("Users");
-
-          var newCollection = db.GetCollection<User>("Users");
-          newCollection.InsertMany(newUserList);
-        }
-      }
-      catch (Exception e)
-      {
-      }
+      var newCollection = db.GetCollection<User>("Users");
+      newCollection.InsertMany(newUserList);
     }
 
     private IMongoDatabase db;
