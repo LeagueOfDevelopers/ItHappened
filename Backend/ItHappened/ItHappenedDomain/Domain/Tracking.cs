83bcea9f8b34500f9b812e77d20bcc45cfcee425
dedalexij <dedalexij@mail.ru>
2018-05-22 20:48:58 +0300

    changed default color

--- Backend/ItHappened/ItHappenedDomain/Domain/Tracking.cs
+++ Backend/ItHappened/ItHappenedDomain/Domain/Tracking.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using MongoDB.Bson.Serialization.Attributes;
@@ -28,7 +28,7 @@ namespace ItHappenedDomain.Domain
       this.dateOfChange = dateOfChange;
       this.isDeleted = isDeleted;
       this.EventCollection = eventCollection;
-      this.color = color ?? "111111";
+      this.color = color ?? "11119017";
     }
 
     public string trackingName { set; get; }
