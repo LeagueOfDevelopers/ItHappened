037ca1d6fb987c5c402df9148eb5968cde647bfd
Unknown <dedalexij@mail.ru>
2018-07-28 17:10:27 +0300

    fixed bug with event change

--- Android/ItHappenedAndroid/app/src/main/java/ru/lod_misis/ithappened/Infrastructure/TrackingRepository.java
+++ Android/ItHappenedAndroid/app/src/main/java/ru/lod_misis/ithappened/Infrastructure/TrackingRepository.java
@@ -144,7 +144,8 @@ public class TrackingRepository implements ITrackingRepository {
             throw new IllegalArgumentException("TrackingV1 with such ID doesn't exists");
 
         realm.beginTransaction();
-        trackingV1.EditEvent(eventId, newScale, newRating, newComment, newDate);
+        TrackingV1 tracking = realm.copyFromRealm(trackingV1);
+        tracking.EditEvent(eventId, newScale, newRating, newComment, newDate);
         realm.commitTransaction();
     }
 
