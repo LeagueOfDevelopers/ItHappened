f70bb4deee540bcdfc3120814c92e0fdccfd8852
Sasha <cu-009@mail.ru>
2019-01-12 09:19:08 +0300

    finished IntroScreen

--- Android/ItHappenedAndroid/app/src/main/java/ru/lod_misis/ithappened/ui/activities/IntroActivity.java
+++ Android/ItHappenedAndroid/app/src/main/java/ru/lod_misis/ithappened/ui/activities/IntroActivity.java
@@ -1,5 +1,6 @@
 package ru.lod_misis.ithappened.ui.activities;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -7,29 +8,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.synnapps.carouselview.CirclePageIndicator;
+
 import java.util.Objects;
 
-import me.relex.circleindicator.CircleIndicator;
+import javax.inject.Inject;
+
 import ru.lod_misis.ithappened.R;
+import ru.lod_misis.ithappened.ui.ItHappenedApplication;
 import ru.lod_misis.ithappened.ui.adapters.ViewPagerAdapter;
 import ru.lod_misis.ithappened.ui.fragments.IntroFragment;
+import ru.lod_misis.ithappened.ui.fragments.IntroLastFragment;
 import ru.lod_misis.ithappened.ui.fragments.StartIntroFragment;
 
 public class IntroActivity extends AppCompatActivity {
 
-    private SharedPreferences sharedPreferences;
+    @Inject
+    SharedPreferences sharedPreferences;
     private ViewPager viewPager;
-    private CircleIndicator circleIndicator;
+    private CirclePageIndicator circleIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        ItHappenedApplication.getAppComponent().inject(this);
+        checkIntro();
         initViews();
         showIntro();
     }
 
     private void initViews() {
+        circleIndicator = findViewById(R.id.circleIndicator);
         viewPager = findViewById(R.id.view_pager);
     }
 
@@ -38,7 +48,15 @@ public class IntroActivity extends AppCompatActivity {
         ((ViewPagerAdapter) adapter).addFragment(new StartIntroFragment());
         ((ViewPagerAdapter) adapter).addFragment(IntroFragment.newInstanse(R.drawable.intro_second_screen));
         ((ViewPagerAdapter) adapter).addFragment(IntroFragment.newInstanse(R.drawable.intro_third_screen));
-        ((ViewPagerAdapter) adapter).addFragment(IntroFragment.newInstanse(R.drawable.intro_fourth_screen));
+        ((ViewPagerAdapter) adapter).addFragment(new IntroLastFragment());
         viewPager.setAdapter(adapter);
+        circleIndicator.setViewPager(viewPager);
+    }
+
+    void checkIntro() {
+        if (sharedPreferences.getBoolean("Tutorial", false)) {
+            Intent intent = new Intent(this, UserActionsActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- Android/ItHappenedAndroid/app/src/main/java/ru/lod_misis/ithappened/ui/fragments/IntroLastFragment.java
+++ Android/ItHappenedAndroid/app/src/main/java/ru/lod_misis/ithappened/ui/fragments/IntroLastFragment.java
@@ -17,7 +17,7 @@ import ru.lod_misis.ithappened.R;
 import ru.lod_misis.ithappened.ui.ItHappenedApplication;
 import ru.lod_misis.ithappened.ui.activities.UserActionsActivity;
 
-public class StartIntroFragment extends Fragment {
+public class IntroLastFragment extends Fragment {
 
     @Inject
     SharedPreferences sharedPreferences;
@@ -25,9 +25,9 @@ public class StartIntroFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.start_intro_screen, container, false);
+        View view = inflater.inflate(R.layout.intro_last_fragment, container, false);
         ItHappenedApplication.getAppComponent().inject(this);
-        Button button = view.findViewById(R.id.miss_tutorial);
+        Button button = view.findViewById(R.id.start_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
